76326813596c5fb2b93de45573f7246104769fd6
Mhz <myckhdez@gmail.com>
2023-10-28 19:01:26 -0600

    Configure los metodos de agregar modifica y eliminar en cliente

--- src/java/Entidades/Cliente.java
+++ src/java/Entidades/Cliente.java
@@ -13,6 +13,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -20,7 +22,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 /**
@@ -29,80 +30,57 @@ import javax.xml.bind.annotation.XmlTransient;
  */
 @Entity
 @Table(name = "Cliente")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c")
-    , @NamedQuery(name = "Cliente.findByClienteId", query = "SELECT c FROM Cliente c WHERE c.clienteId = :clienteId")
-    , @NamedQuery(name = "Cliente.findByNombres", query = "SELECT c FROM Cliente c WHERE c.nombres = :nombres")
-    , @NamedQuery(name = "Cliente.findByApellidos", query = "SELECT c FROM Cliente c WHERE c.apellidos = :apellidos")
-    , @NamedQuery(name = "Cliente.findByIdentificacion", query = "SELECT c FROM Cliente c WHERE c.identificacion = :identificacion")
-    , @NamedQuery(name = "Cliente.findByCorreoElectronico", query = "SELECT c FROM Cliente c WHERE c.correoElectronico = :correoElectronico")
-    , @NamedQuery(name = "Cliente.findByNumeroTelefonico", query = "SELECT c FROM Cliente c WHERE c.numeroTelefonico = :numeroTelefonico")})
 public class Cliente implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    
     @Id
-    @Basic(optional = false)
-    @NotNull
     @Column(name = "cliente_id")
-    private BigDecimal clienteId;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long clienteId;
     @Basic(optional = false)
+    
     @NotNull
-    @Size(min = 1, max = 100)
-    @Column(name = "nombres")
-    private String nombres;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 100)
-    @Column(name = "apellidos")
+    @Size(min=1,max=100)
+    @Column(name="apellidos")
     private String apellidos;
-    @Basic(optional = false)
+    
     @NotNull
-    @Column(name = "identificacion")
-    private int identificacion;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 100)
-    @Column(name = "correoElectronico")
+    @Size(min=1,max=100)
+    @Column(name="correoElectronico")
     private String correoElectronico;
-    @Basic(optional = false)
+    
     @NotNull
-    @Column(name = "numeroTelefonico")
-    private int numeroTelefonico;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "clienteId", fetch = FetchType.LAZY)
-    private List<Reserva> reservaList;
+    @Column(name="identificacion")
+    private Integer identificacion;
+    
+    @NotNull
+    @Size(min=1,max=100)
+    @Column(name="nombres")
+    private String nombres;
+    
+    @NotNull
+    @Column(name="numeroTelefonico")
+    private Integer numeroTelefonico;
 
     public Cliente() {
     }
 
-    public Cliente(BigDecimal clienteId) {
+    public Cliente(long clienteId, String apellidos, String correoElectronico, Integer identificacion, String nombres, Integer numeroTelefonico) {
         this.clienteId = clienteId;
-    }
-
-    public Cliente(BigDecimal clienteId, String nombres, String apellidos, int identificacion, String correoElectronico, int numeroTelefonico) {
-        this.clienteId = clienteId;
-        this.nombres = nombres;
         this.apellidos = apellidos;
-        this.identificacion = identificacion;
         this.correoElectronico = correoElectronico;
+        this.identificacion = identificacion;
+        this.nombres = nombres;
         this.numeroTelefonico = numeroTelefonico;
     }
 
-    public BigDecimal getClienteId() {
+    public long getClienteId() {
         return clienteId;
     }
 
-    public void setClienteId(BigDecimal clienteId) {
+    public void setClienteId(long clienteId) {
         this.clienteId = clienteId;
-    }
-
-    public String getNombres() {
-        return nombres;
-    }
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
     }
 
     public String getApellidos() {
@@ -113,14 +91,6 @@ public class Cliente implements Serializable {
         this.apellidos = apellidos;
     }
 
-    public int getIdentificacion() {
-        return identificacion;
-    }
-
-    public void setIdentificacion(int identificacion) {
-        this.identificacion = identificacion;
-    }
-
     public String getCorreoElectronico() {
         return correoElectronico;
     }
@@ -129,46 +99,28 @@ public class Cliente implements Serializable {
         this.correoElectronico = correoElectronico;
     }
 
-    public int getNumeroTelefonico() {
+    public Integer getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(Integer identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public Integer getNumeroTelefonico() {
         return numeroTelefonico;
     }
 
-    public void setNumeroTelefonico(int numeroTelefonico) {
+    public void setNumeroTelefonico(Integer numeroTelefonico) {
         this.numeroTelefonico = numeroTelefonico;
-    }
-
-    @XmlTransient
-    public List<Reserva> getReservaList() {
-        return reservaList;
-    }
-
-    public void setReservaList(List<Reserva> reservaList) {
-        this.reservaList = reservaList;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (clienteId != null ? clienteId.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Cliente)) {
-            return false;
-        }
-        Cliente other = (Cliente) object;
-        if ((this.clienteId == null && other.clienteId != null) || (this.clienteId != null && !this.clienteId.equals(other.clienteId))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Entidades.Cliente[ clienteId=" + clienteId + " ]";
     }
     
 }
